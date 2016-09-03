e3035380daca937737b4c1be1247ace894b597d0
Namaan Hussain <xxthekillerwithinxx1712@outlook.com>
2016-09-03 12:24:55 +0100

    Added clear ram cache function & button.

--- XxRaPiDK3LLERxX's App/Main_Menu.Designer.vb
+++ XxRaPiDK3LLERxX's App/Main_Menu.Designer.vb
@@ -22,8 +22,35 @@ Partial Class Main_Menu
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(2, 694)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(186, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Clear RAM cache"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Main_Menu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Main_Menu"
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1264, 729)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Main_Menu"
+        Me.ShowIcon = False
+        Me.Text = "Main Menu"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
--- XxRaPiDK3LLERxX's App/Main_Menu.vb
+++ XxRaPiDK3LLERxX's App/Main_Menu.vb
@@ -1,3 +1,23 @@
 ﻿Public Class Main_Menu
+    Dim myProcess As Process
+    Dim myProcesses As Process() = Process.GetProcessesByName("ApplicationName")
+    Declare Function SetProcessWorkingSetSize Lib "kernel32.dll" (ByVal process As IntPtr, ByVal minimumWorkingSetSize As Integer, ByVal maximumWorkingSetSize As Integer) As Integer
+    Private Sub Main_Menu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+    Public Shared Sub FlushMemory()
+
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
+        If (Environment.OSVersion.Platform = PlatformID.Win32NT) Then
+            SetProcessWorkingSetSize(Process.GetCurrentProcess().Handle, -1, -1)
+        End If
+        'Dim ProcessInfo As Process For Each myProcess In myProcesses SetProcessWorkingSetSize(myProcess.Handle, -1, -1) Next myProcess End If Catch ex As Exception End Try End Sub
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FlushMemory()
+
+    End Sub
 End Class
